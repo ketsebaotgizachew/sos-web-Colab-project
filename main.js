4fe04da273aab7b2f18fddb7e06fcf8e1c274424
ketsebaotgizachew <112580228+ketsebaotgizachew@users.noreply.github.com>
2023-10-05 12:34:01 +0300

    First Commit
    Download Arge

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    
+    setTimeout(() => {
+        let trc = document.getElementById("trc")
+        let mdl = document.getElementById("mdl")
+        let links = document.getElementById("links")
+        mdl.style.top = "15%"
+        mdl.style.left = "-20%"
+        trc.style.top = "-37%"
+        trc.style.left = "84%"
+        links.style.marginTop = "0px"
+    }, 500);
+    
+}
